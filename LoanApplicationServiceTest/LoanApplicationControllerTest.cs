989aa8d31c14ca12bd877aa3b667898946a74cf0
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2025-03-23 16:55:30 +1100

    Updated the README file

--- LoanApplicationServiceTest/LoanApplicationControllerTest.cs
+++ LoanApplicationServiceTest/LoanApplicationControllerTest.cs
@@ -4,11 +4,7 @@ using LoanApplicationService.RabbitMq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace LoanApplicationServiceTest
 {
